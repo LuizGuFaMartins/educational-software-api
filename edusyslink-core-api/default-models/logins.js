ffa897afcced79b0638fbb32daedcd153b347756
LuizFarabello <luizgufamartins@gmail.com>
2023-10-07 11:35:42 -0300

    first commit

---
+++ edusyslink-core-api/default-models/logins.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+const database = require("../resources/database");
+
+const Login = database.define("logins", {
+  login_id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  login_email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  login_password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  login_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  login_photo_url: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  login_type: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+});
+
+Login.sync();
+
+module.exports = Login;
